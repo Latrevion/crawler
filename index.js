b38744fbe3723d96fd815c27f3de36e46b684107
Latrevion <xiaopikaqiu@outlook.com>
2022-07-10 13:11:35 +0800

    完善请求，并使用open打开

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fetch = require("node-fetch")
 const fs = require("fs")
 const path = require("path")
 const {prompt} = require("enquirer")
+const http = require("http")
+const handler = require("serve-handler")
+const open = require('open')
 
 // fetch("https://www.nowcoder.com/search?type=post&order=recall&query=%E8%85%BE%E8%AE%AF+%E5%89%8D%E7%AB%AF&subType=0&tagId=&page=2")
 //   .then(res => res.text())
@@ -23,7 +26,7 @@ async function start() {
     message: "下载数量",
     initial: 30
   })
-  result.count =parseInt(response.count)
+  result.count = parseInt(response.count)
 
 
   response = await prompt({
@@ -54,18 +57,29 @@ async function start() {
 
     urls = body.match(/\/discuss\/\d+\?.+?"/g) || []
 
-    urls = urls.map(text => `https://www.nowcoder.com${text.replace('"', '')}`)
+    urls = urls.map(text => `https://www.nowcoder.com${text.replace("\"", "")}`)
 
     for (let i = 0; i < urls.length; i++) {
+      console.log(`下载第${i}个`)
       fs.writeFileSync(path.join(process.cwd(), `${result.job}_${result.company}_${count}.html`), body, "utf-8")
       count++
-      await new Promise(resolve => setTimeout(resolve,500))
+
+      if(count >=result.count)break
+      //降低请求频率，做个延时
+      await new Promise(resolve => setTimeout(resolve, 500))
     }
     page++
-    console.log(count,result.count, urls.length,page)
-    console.log(count < result.count && urls.length>0)
-  } while (count < result.count && urls.length>0)
+    // console.log(count, result.count, urls.length, page)
+    // console.log(count < result.count && urls.length > 0)
+  } while (count < result.count && urls.length > 0)
+  console.log("下载完成")
 
+  http.createServer(async (req, res)=>{
+    await handler(req, res)
+  }).listen(3000,async ()=>{
+    await open('http://localhost:3000')
+  })
+  // console.log('打开 http://localhost:8080')
 }
 
 start()
